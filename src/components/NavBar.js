5c4835e41b0e6cb1a6b01a0115f64bba422b4cf1
maria fernanda goracy <mfernandagoracy@gmail.com>
2020-08-22 01:02:20 -0300

    Agrego ícono font awesome

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
@@ -8,8 +10,11 @@ const NavBar = () => {
         <Navbar.Brand href="#home">Nombre tienda</Navbar.Brand>
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Productos</Nav.Link>
-          <Nav.Link href="#pricing">Carrito</Nav.Link>
+          <Nav.Link href="#productos">Productos</Nav.Link>
+          <Nav.Link href="#carrito">
+            <FontAwesomeIcon className="mr-2 ml-2" icon={faShoppingCart} />
+            Carrito
+          </Nav.Link>
         </Nav>
       </Navbar>
     </>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from './App';
-import NavBar from "./components/NavBar";
+import App from "./App";
+//import NavBar from "./components/NavBar";
+//import Home from "./pages/Home";
 import * as serviceWorker from "./serviceWorker";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
