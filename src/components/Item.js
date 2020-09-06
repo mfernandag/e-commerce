989e7e4fc628be36c4ca8cff4774fefac5d26dc9
maria fernanda goracy <mfernandagoracy@gmail.com>
2020-09-05 22:03:14 -0300

    setTimeout con promise

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,36 +1,67 @@
-import React from "react";
-import itemList from "./ItemList";
+import React, { useState } from "react";
+import { itemList } from "./ItemList";
 import { Card, Button, Container } from "react-bootstrap";
 
 const Item = () => {
-  return (
-    <Container className="d-flex flex-wrap">
-      {itemList.map((item, id) => {
-        return (
-          <Card key={id} style={{ width: "18rem" }}>
-            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>{item.description}</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </Container>
+  const [productsList, setProductsList] = useState([]);
+  const [error, setError] = useState(
+    "No hay productos disponibles en este momento"
   );
+
+  itemList()
+    .then((productList) => {
+      setProductsList(productList);
+    })
+    .catch((err) => {
+      setError(err);
+    });
+
+  if (!error) {
+    return (
+      <Container className="d-flex justify-content-around flex-wrap mt-4">
+        {productsList.map((item, id) => {
+          return (
+            <Card className="mb-4" key={id} style={{ width: "18rem" }}>
+              {/* <Card.Img variant="top" src={poppyImg} /> */}
+              <Card.Img variant="top" src={item.img} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>{item.description}</Card.Text>
+                <Button variant="success">Agregar al carrito</Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </Container>
+    );
+  } else return <span>{error}</span>;
+
+  // const [showProductsList, setProductsList] = useState(false);
+  // useEffect(() => {
+  //   let timer1 = setTimeout(() => setProductsList(itemList), 2000);
+  //   return () => {
+  //     clearTimeout(timer1);
+  //   };
+  // }, []);
+
+  // return (
+  //   showProductsList && (
+  //     <Container className="d-flex justify-content-around flex-wrap">
+  //       {itemList.map((item, id) => {
+  //         return (
+  //           <Card className="mb-4" key={id} style={{ width: "18rem" }}>
+  //             {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+  //             <Card.Body>
+  //               <Card.Title>{item.name}</Card.Title>
+  //               <Card.Text>{item.description}</Card.Text>
+  //               <Button variant="primary">Go somewhere</Button>
+  //             </Card.Body>
+  //           </Card>
+  //         );
+  //       })}
+  //     </Container>
+  //   )
+  // );
 };
 
 export default Item;
-
-// {/* <Card style={{ width: "18rem" }}>
-//         {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-//         <Card.Body>
-//           <Card.Title>Card Title</Card.Title>
-//           <Card.Text>
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </Card.Text>
-//           <Button variant="primary">Go somewhere</Button>
-//         </Card.Body>
-//       </Card> */}
