fe3be020e3cb499a1d8ab11d1b803d127aaccc5b
mfernandag <mfernandagoracy@gmail.com>
2020-10-26 21:52:28 -0300

    Agrego gif, cambios en .env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDyWKVvY1-E3mgpWK9tdd8xw7v2DrHTp8Y",
-  authDomain: "e-commerce-8fe98.firebaseapp.com",
-  databaseURL: "https://e-commerce-8fe98.firebaseio.com",
-  projectId: "e-commerce-8fe98",
-  storageBucket: "e-commerce-8fe98.appspot.com",
-  messagingSenderId: "988149036121",
-  appId: "1:988149036121:web:058bf7f2b8164fe59f6d48",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export function getFirebase() {
